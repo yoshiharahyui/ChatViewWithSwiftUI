cf6aea7781303094149ebd4fb0798ae2a1c15537
yoshiharahyui <1015hyui@gmail.com>
2024-08-09 21:28:13 +0900

    gitignore

---
+++ ChatViewWithSwiftUI/ChatViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUIApp.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by 吉原飛偉 on 2024/08/09.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatViewWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by 吉原飛偉 on 2024/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
