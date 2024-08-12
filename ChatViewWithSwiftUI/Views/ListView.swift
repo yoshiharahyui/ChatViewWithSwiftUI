3991d469710de041223e7a8d80bb2acf046860bc
yoshiharahyui <1015hyui@gmail.com>
2024-08-12 16:03:29 +0900

    大まかなレイアウト作成

--- ChatViewWithSwiftUI/Views/ListView.swift
+++ ChatViewWithSwiftUI/Views/ListView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            //Header
+            HStack {
+                Text("トーク")
+                    .font(.title2.bold())
+                Spacer()
+                HStack {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Circle()
+                        .frame(width: 40, height: 40)
+                }
+            }
+            
+            //List
+            ScrollView {
+                VStack {
+                    ForEach(0..<5) { _ in
+                        HStack {
+                            Circle()
+                                .frame(width: 60, height: 60)
+                            VStack(alignment: .leading) {
+                                Text("タイトル")
+                                Text("最新のメッセージ")
+                                    .font(.footnote)
+                                    .foregroundColor(Color(uiColor: .secondaryLabel))
+                            }
+                            Spacer()
+                            Text("12/31")
+                                .font(.caption)
+                                .foregroundColor(Color(uiColor: .secondaryLabel))
+                        }
+                    }
+                }
+            }
+            
+        }
+        .padding(.horizontal)
     }
 }
 
