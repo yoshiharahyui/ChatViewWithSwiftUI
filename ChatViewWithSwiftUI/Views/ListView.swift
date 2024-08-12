cef2fe747f4d667993c38aeb4e651b71d965b825
yoshiharahyui <1015hyui@gmail.com>
2024-08-12 15:48:06 +0900

    リスト用のファイル作成

---
+++ ChatViewWithSwiftUI/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by 吉原飛偉 on 2024/08/12.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
