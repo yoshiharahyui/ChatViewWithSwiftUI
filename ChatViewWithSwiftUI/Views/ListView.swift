1d91a1fb8a550a96ff6665d1367784e397c07bba
yoshiharahyui <1015hyui@gmail.com>
2024-08-12 16:27:29 +0900

    コードの整理

--- ChatViewWithSwiftUI/Views/ListView.swift
+++ ChatViewWithSwiftUI/Views/ListView.swift
@@ -11,41 +11,10 @@ struct ListView: View {
     var body: some View {
         VStack {
             //Header
-            HStack {
-                Text("トーク")
-                    .font(.title2.bold())
-                Spacer()
-                HStack {
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Circle()
-                        .frame(width: 40, height: 40)
-                }
-            }
+            header
             
             //List
-            ScrollView {
-                VStack {
-                    ForEach(0..<5) { _ in
-                        HStack {
-                            Circle()
-                                .frame(width: 60, height: 60)
-                            VStack(alignment: .leading) {
-                                Text("タイトル")
-                                Text("最新のメッセージ")
-                                    .font(.footnote)
-                                    .foregroundColor(Color(uiColor: .secondaryLabel))
-                            }
-                            Spacer()
-                            Text("12/31")
-                                .font(.caption)
-                                .foregroundColor(Color(uiColor: .secondaryLabel))
-                        }
-                    }
-                }
-            }
+            list
             
         }
         .padding(.horizontal)
@@ -54,4 +23,49 @@ struct ListView: View {
 
 #Preview {
     ListView()
+}
+
+extension ListView {
+    private var header: some View {
+        HStack {
+            Text("トーク")
+                .font(.title2.bold())
+            Spacer()
+            HStack {
+                Circle()
+                    .frame(width: 40, height: 40)
+                Circle()
+                    .frame(width: 40, height: 40)
+                Circle()
+                    .frame(width: 40, height: 40)
+            }
+        }
+    }
+    
+    private var list: some View {
+        ScrollView {
+            VStack {
+                ForEach(0..<5) { _ in
+                    listRow
+                }
+            }
+        }
+    }
+    
+    private var listRow: some View {
+        HStack {
+            Circle()
+                .frame(width: 60, height: 60)
+            VStack(alignment: .leading) {
+                Text("タイトル")
+                Text("最新のメッセージ")
+                    .font(.footnote)
+                    .foregroundColor(Color(uiColor: .secondaryLabel))
+            }
+            Spacer()
+            Text("12/31")
+                .font(.caption)
+                .foregroundColor(Color(uiColor: .secondaryLabel))
+        }
+    }
 }
